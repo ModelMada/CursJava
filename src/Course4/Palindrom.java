7bc9c1e1a3573fe3f7c2dd3c1f9b4ada7d10dc6a
unknown <ali.suican@gmail.com>
2023-05-22 09:10:38 +0300

    palidrom

--- src/Course4/Palindrom.java
+++ src/Course4/Palindrom.java
@@ -5,8 +5,6 @@ public class Palindrom {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 checkPalindrom("radar");
-		
-
 	}
 
 	public static void checkPalindrom(String cuvant) {
@@ -18,11 +16,13 @@ public class Palindrom {
 		 * cuvant intors = cuvantIntors + "r" ...
 		 * 
 		 */
+		
 		String cuvantIntors = "";
 		
 		for (int i = cuvant.length()-1 ; i>=0 ;i--) {
 			cuvantIntors = cuvantIntors + cuvant.charAt(i);
 		}
+		
 		String result = cuvantIntors.equals(cuvant)? "Este palindrom": "nu este palindrom";
 		System.out.println(result);
 		
